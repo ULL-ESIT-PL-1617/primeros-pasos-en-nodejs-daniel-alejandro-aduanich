bd660c51a8e86185c88e0fae5a9d0f95229e2b5a
dariasteam <dariasteam94@gmail.com>
2017-02-22 08:43:48 +0000

    Esqueleto del gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@ var gulp = require('gulp');
 var ghpages = require('gh-pages');
 var path = require('path');
 
-
-gulp.task('gh-pages', function(){
+gulp.task('deploy', function(){
 	ghpages.publish(path.join(__dirname, '_book'), {
     message: 'Auto update gh-pages branch'
   }, function(err) {});
 });
 
-gulp.task('default', ['gh-pages' ]);
+gulp.task('build', function(){
+
+});
+
+gulp.task('serve', function(){
+
+});
+
+gulp.task('default', []);
