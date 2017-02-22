16aaee241ef44219fe8631538add386c97e1f5c2
dariasteam <dariasteam94@gmail.com>
2017-02-22 11:19:33 +0000

    wiki deploy prueba 1

--- scripts/deploy-wiki.js
+++ scripts/deploy-wiki.js
@@ -2,9 +2,17 @@
 
 var path = require('path');
 var fs = require('fs-extra');
+var init = require('git-init')
+var shell = require('shelljs');
+var obj = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 
 main();
 
 function main() {
   fs.removeSync ('wiki/.git');
+  shell.cd("wiki/");
+  shell.exec("git init .");
+  shell.exec("git add .");
+  shell.exec("git commit -m 'Deploy to wiki'");
+  shell.exec("git remote add origin " + obj.repository.wiki);
 }
