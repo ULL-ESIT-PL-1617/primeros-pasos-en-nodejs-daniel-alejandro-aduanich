aab62723a48f8c081d6f150cd6244100d3c91b94
alu0100812534 <alu0100812534@ull.edu.es>
2017-02-23 01:12:07 +0000

    bug2

--- p3-express.js
+++ p3-express.js
@@ -19,6 +19,6 @@ var server = app.listen(80, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+  console.log('Escuchando en http://%s:%s', host, port)
 
 })
